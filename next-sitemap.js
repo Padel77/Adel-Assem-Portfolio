d269f4ee6f13a79ea404cfeb4290603ec9857442
Padel77 <adelassem99@gmail.com>
2024-10-23 00:17:16 +0300

    solve Unable to find next-sitemap.config.js

--- next-sitemap.js
+++ next-sitemap.js
@@ -1,15 +1,15 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: 'https://adel-assem-portfolio.vercel.app', // Replace with your site URL
-    generateRobotsTxt: true, // (optional) Generate a robots.txt file
-    sitemapSize: 5000, 
-    changefreq: 'daily', // (optional) Change frequency of the URLs
-    priority: 0.7, // (optional) Priority of the URLs
-    exclude: ['/admin/*'], // (optional) Exclude specific paths
-    robotsTxtOptions: {
-      additionalSitemaps: [
-        'https://adel-assem-portfolio.vercel.app/ar',
-        'https://adel-assem-portfolio.vercel.app/en',
-      ],
-    },
-  };
+  siteUrl: 'https://adel-assem-portfolio.vercel.app', // Replace with your site URL
+  generateRobotsTxt: true, // (optional) Generate a robots.txt file
+  sitemapSize: 5000,
+  changefreq: 'daily', // (optional) Change frequency of the URLs
+  priority: 0.7, // (optional) Priority of the URLs
+  exclude: ['/admin/*'], // (optional) Exclude specific paths
+  robotsTxtOptions: {
+    additionalSitemaps: [
+      'https://adel-assem-portfolio.vercel.app/ar',
+      'https://adel-assem-portfolio.vercel.app/en',
+    ],
+  },
+};
