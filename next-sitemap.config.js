c270d6ca382dff5c776e53c7109524e444e85a2a
Padel77 <adelassem99@gmail.com>
2024-10-23 00:38:28 +0300

    solve Unable to find next-sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   siteUrl: 'https://adel-assem-portfolio.vercel.app', // Replace with your site URL
   generateRobotsTxt: true, // (optional) Generate a robots.txt file
-  sitemapSize: 5000,
+  sitemapSize: 5000, 
   changefreq: 'daily', // (optional) Change frequency of the URLs
   priority: 0.7, // (optional) Priority of the URLs
   exclude: ['/admin/*'], // (optional) Exclude specific paths
